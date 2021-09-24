0594d2f29646f65db22a60102d25c7aa675e9cae
Lee Lup Yuen <luppy@appkaki.com>
2021-09-24 13:47:02 +0800

    Don't use memstream

--- bouffalo.mk
+++ bouffalo.mk
@@ -10,3 +10,10 @@ COMPONENT_OBJS := $(patsubst %.c,%.o, $(COMPONENT_SRCS))
 
 # Source Folders
 COMPONENT_SRCDIRS := src
+
+# Define the GCC compiler options:
+# CFLAGS for C compiler, CPPFLAGS for C++ compiler
+
+# Don't use memstream.
+# See src/cbortojson.c
+CFLAGS   += -DWITHOUT_OPEN_MEMSTREAM
